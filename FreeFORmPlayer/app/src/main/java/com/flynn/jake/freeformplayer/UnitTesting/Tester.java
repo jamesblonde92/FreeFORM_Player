ed070ee52e882268081506dc13e5eb0f3e2ee849
Shane Olson <yobocs@gmail.com>
2015-12-08 19:32:24 -0800

    Created Unit tests. Song object is tested!

---
+++ FreeFORmPlayer/app/src/main/java/com/flynn/jake/freeformplayer/UnitTesting/Tester.java
@@ -0,0 +1,39 @@
+package com.flynn.jake.freeformplayer.UnitTesting;
+
+import android.net.Uri;
+
+import com.flynn.jake.freeformplayer.models.Song;
+
+
+/**
+ * Created by Shane on 12/8/2015.
+ */
+public class Tester {
+
+    public static void main(String args[]){
+
+
+        if (testSongObject()){
+            System.out.print("Song object is storing data and functioning according to expectations.");
+        }
+        else{
+            System.out.print("Song object is not functioning properly");
+        }
+    }
+
+    public static boolean testSongObject(){
+
+        Song mySong = new Song((long)100, "Stitches", "Pop", "Shawn Mendes", "Unknown", 2015, null);
+        String mySongName = mySong.getName();
+        long mySongID =  mySong.getSongID();
+        //String mySongGenre = mySong.getGenre();
+        String mySongArtist = mySong.getArtist();
+        String mySongAlbum = mySong.getAlbum();
+        int mySongYear = mySong.getYear();
+
+        if(mySongName != null && mySongID != 0 && mySongArtist != null && mySongAlbum != null && mySongYear != 0)
+            return true;
+        else
+            return false;
+    }
+}
